7e16b64a20382aaf82a38d69dfe17a41665fc5dd
gurjas14 <106869496+gurjas14@users.noreply.github.com>
2022-06-07 16:47:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("Simple caculator")
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+
+sum= a+b #sum of a and b
+difference= a-b # difference of a and b
+product= a*b #product of a and b
+divide=a/b #division of and b
+
+print("The sum of a and b", sum) # printing the sum
+print("The difference of a and b", difference) #printing the difference
+print("The product of a and b",product)# printing the product 
+print("the division of a and b", divide) # printing the division
+2
+
+
